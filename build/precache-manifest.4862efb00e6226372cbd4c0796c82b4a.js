ca9bbc4b67acffd1c36bce0cb7b0e41f6e3467c5
robefernandez22 <robertofernandezromero61@gmail.com>
2022-10-28 13:36:02 +0200

    build para despliegue en github pages

---
+++ build/precache-manifest.4862efb00e6226372cbd4c0796c82b4a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0f37e2bcd08ad93bed18",
+    "url": "/robefernandez22/curso-intro-react/static/css/main.741e8430.chunk.css"
+  },
+  {
+    "revision": "0f37e2bcd08ad93bed18",
+    "url": "/robefernandez22/curso-intro-react/static/js/main.0f37e2bc.chunk.js"
+  },
+  {
+    "revision": "bc22eccc40ad1be876d9",
+    "url": "/robefernandez22/curso-intro-react/static/js/1.bc22eccc.chunk.js"
+  },
+  {
+    "revision": "7ec51e4ac9d521819833",
+    "url": "/robefernandez22/curso-intro-react/static/js/runtime~main.7ec51e4a.js"
+  },
+  {
+    "revision": "8c7a858673065169071dbce1595d72cc",
+    "url": "/robefernandez22/curso-intro-react/index.html"
+  }
+];
